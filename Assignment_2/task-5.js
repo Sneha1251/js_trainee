4201f65842e1d1af062bb817017fea5bf61948ba
Sneha/successive <161453807+Sneha1251@users.noreply.github.com>
2024-03-05 16:09:02 +0530

    update directory

--- Assignment_2/task-5.js
+++
@@ -1,7 +0,0 @@
-// Write a function expression that takes in two numbers and returns their sum.
-
-const sum = function (first,second){
-    return first + second;
-};
-
-console.log("Sum of number is ",sum(5,6));
