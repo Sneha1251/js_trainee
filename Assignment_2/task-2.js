9ff82758bcffa46aeae86a71115a9f1edb3c5548
Sneha/successive <sneha.gupta@successive.tech>
2024-03-05 15:50:10 +0530

    assignment 2 completed

---
+++ Assignment_2/task-2.js
@@ -0,0 +1,7 @@
+// Write a function to take input(age, name, phone no.) from user and display it
+
+let fullname= prompt("Enter the Full name");
+let age= prompt("Enter the Age");
+let no= prompt("Enter the Phone No");
+
+console.log("The name is ",fullname,", age is ",age," and phone no is ",no);
