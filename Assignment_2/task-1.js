26e9cd799a4f3eef6749dec8b44b642fa8406a12
Sneha/successive <sneha.gupta@successive.tech>
2024-03-07 10:38:34 +0530

    commit assignment 3

--- Assignment_2/task-1.js
+++
@@ -1,3 +0,0 @@
-// Write a program to give a random output between 1 to 10
-
-console.log(Math.floor(Math.random() * 10)+1)
--- Assignment_2/task-4.js
+++
@@ -1,7 +0,0 @@
-// Write a function expression that takes in a number and returns its square.
-
-const square = function (length){
-    return length * length;
-};
-
-console.log("Sqaure of number is ",square(5));
