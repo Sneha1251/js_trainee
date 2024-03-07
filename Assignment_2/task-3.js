1124c96056e67dc9475c6731dd13f1ded6e1f994
Sneha/successive <sneha.gupta@successive.tech>
2024-03-07 10:21:54 +0530

    Added assignment 3

---
+++ Assignment_2/task-3.js
@@ -0,0 +1,10 @@
+// Write a function expression that takes in another function as an argument
+
+function inner(){
+    return 'hello world!';
+}
+function outer(inner){
+    console.log(inner());
+}
+ 
+outer(inner);
---
+++ Assignment_3/task-9.js
@@ -0,0 +1,12 @@
+// Write a program to print fibonaaci series
+let i=0;
+console.log(i);
+let j=1;
+console.log(j);
+let k;
+for(let num=1;num<50;num++){
+  k=i+j;
+  console.log(k);
+  i=j;
+  j=k; 
+}
