bdc4710a494b5b8ac31116157a053e13b698e153
Sneha/successive <sneha.gupta@successive.tech>
2024-03-08 12:32:04 +0530

    Testing

---
+++ Assignment-5/task-1.js
@@ -0,0 +1,12 @@
+//  Write a program to iterate over object.
+
+const exampleObj = {
+  book: "Sherlock Holmes",
+  author: "Arthur Conan Doyle",
+  genre: "Mystery",
+};
+
+for (let keys in exampleObj) {
+  let value = exampleObj[keys];
+  console.log(`${keys}:${value}`);
+}
