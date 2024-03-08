cfbdbd2eecc24f73382bc9392978c2a6b1e2f654
Sneha/successive <sneha.gupta@successive.tech>
2024-03-08 15:57:56 +0530

    Completed assignment 5

---
+++ Assignment-5/task-3.js
@@ -0,0 +1,16 @@
+// Write a program to remove all the id key from array of object ({name:"John", age:27, id:1})
+
+const studentObj = [
+  { name: "Sneha", age: 22, id: "1234" },
+  { name: "Parth", age: 20, id: "1235" },
+  { name: "Pritam", age: 23, id: "1236" },
+  { name: "Raksha", age: 21, id: "1237" },
+];
+
+let removedIdObject = studentObj.filter((obj) => {
+  return delete obj.id;
+});
+
+console.log(removedIdObject);
+
+
