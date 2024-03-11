28f23310f2282544023fdee7965e961d652fef58
Sneha/successive <sneha.gupta@successive.tech>
2024-03-11 11:12:14 +0530

    Updated code

--- Assignment-3/task-4.js
+++ Assignment-3/task-4.js
@@ -1,12 +1,15 @@
 // Write a program to print all even number first and then all odd numbers
-
-for(let i=0;i<100;i++){
-    if(i%2==0){
-        console.log("Even no",i);
-    }
+let evenNumbers = [];
+let oddNumbers = [];
+for (let i = 0; i < 50; i++) {
+  if (i % 2 == 0) {
+    evenNumbers.push(i);
+  }
 }
-for(let i=0;i<100;i++){
-    if(i%2!=0){
-        console.log("Odd no",i);
-    }
+console.log("Even no", evenNumbers);
+for (let i = 0; i < 50; i++) {
+  if (i % 2 != 0) {
+    oddNumbers.push(i);
+  }
 }
+console.log("Odd no", oddNumbers);
--- Assignment-3/task-6.js
+++ Assignment-3/task-6.js
@@ -1,13 +1,13 @@
 //Write a program to display prime numbers from 1 to 50
-let flag=false;
-for(let i=2;i<=50;i++){
-    flag=false;
-    for(let j=2;j<=i/2;j++){
-        if(i%j==0){
-            flag=true;
-        }
+let flag = false;
+for (let i = 2; i <= 50; i++) {
+  flag = false;
+  for (let j = 2; j <= i / 2; j++) {
+    if (i % j == 0) {
+      flag = true;
     }
-    if(flag==false){
-        console.log("prime no ",i);
-    }
+  }
+  if (flag == false) {
+    console.log("prime no ", i);
+  }
 }
